44d0c854437b2d8b2ca58c6c457267540b318aaf
Ubuntu <ubuntu@ip-172-31-19-239.eu-central-1.compute.internal>
2017-08-28 20:53:39 +0000

    first commit

---
+++ PyEventEmitter.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+import collections
+
+class PyEventEmitter:
+	
+	def __init__(self):
+		self._arr=collections.defaultdict(list)
+
+	def on(self,event):
+		def _on(evthandler):
+			self._arr[event].append(evthandler)
+			print (len(self._arr[event])," listeners for this event")
+		return _on
+
+
+	def emit(self,event,data=None):
+		for f in self._arr[event]:
+			f(data)
+		
+	def getListeners(self,event):
+		return self._arr[event]
+
+	def getEventsList(self):
+		return list(self._arr.keys())
+
+	def getEventsCount(self):
+		return len(self._arr)
+
+	def delListenerByName(self,event,fname):
+		eventscopy=self._arr[event]
+		for i in eventscopy:
+			if i.__name__==fname:
+				self._arr[event].remove(i)	
+	def delAllListeners(self,event):
+		del self._arr[event]
+
+
+
